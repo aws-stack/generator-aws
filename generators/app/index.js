63028ecaf44e1e967171710de3c79adf2ebcd5e2
Filipe Niero Felisbino <filipenf@gmail.com>
2017-04-26 20:17:03 -0300

    Re-order regions / improve questions

--- generators/app/index.js
+++ generators/app/index.js
@@ -2,20 +2,20 @@ var Generator = require('yeoman-generator');
 var CIDR = require('node-cidr');
 
 var AWS_REGIONS = [
-        "ap-south-1",
-        "eu-west-2",
-        "eu-west-1",
-        "ap-northeast-2",
-        "ap-northeast-1",
-        "sa-east-1",
-        "ca-central-1",
-        "ap-southeast-1",
-        "ap-southeast-2",
-        "eu-central-1",
         "us-east-1",
         "us-east-2",
         "us-west-1",
-        "us-west-2"
+        "us-west-2",
+        "eu-west-1",
+        "eu-west-2",
+        "eu-central-1",
+        "ca-central-1",
+        "ap-south-1",
+        "ap-northeast-1",
+        "ap-northeast-2",
+        "ap-southeast-1",
+        "ap-southeast-2",
+        "sa-east-1"
     ];
 
 function validate_region(region) {
